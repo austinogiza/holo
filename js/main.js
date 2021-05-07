26a94d25f93924a8a90467ff67c249ac8a32b91e
austinogiza <austinogiza@gmail.com>
2021-05-07 10:56:03 +0100

    hamburger navigation done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".toggle_menu")
+const stack = document.querySelector(".toggle_stack")
+const toggle = document.querySelectorAll(".toggle")
+const sidebar = document.querySelector(".sidebar")
+
+
+const toggleMenu =()=>{
+
+    sidebar.classList.toggle("nav_active")
+    stack.classList.toggle('tog')
+
+}
+
+menu.addEventListener('click', toggleMenu)
